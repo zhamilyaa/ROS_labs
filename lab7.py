3feac1552e840dd30bb089f19e7ec17510e584a5
Zhamilya <40009146+zhamilyaa@users.noreply.github.com>
2021-11-28 01:07:23 +0600

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -1,9 +1,11 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from keras import backend as K
 
 def main():
     data = pd.read_csv("./dict1.csv", header = None, names = ["Angles", "XY"])
@@ -41,6 +43,8 @@ def main():
     print(np.shape(X_test))
     print(np.shape(y_test))
 
+    def rmse(y_true, y_pred):
+        return K.sqrt(K.mean(K.square(y_pred - y_true)))
 
     model = Sequential()
     model.add(Dense(10, input_dim =3, activation = 'tanh'))
@@ -50,22 +54,34 @@ def main():
     model.add(Dense(16, activation = 'tanh'))
     model.add(Dense(16, activation = 'tanh'))
     model.add(Dense(16, activation = 'tanh'))
-    model.add(Dense(16, activation = 'tanh'))
-    model.add(Dense(16, activation = 'tanh'))
+
     model.add(Dense(5, activation='linear'))
     model.compile(loss='mean_squared_logarithmic_error', optimizer=keras.optimizers.Adam(0.01))
 
-
-    model.fit(X_train, y_train, epochs = 200)
+    epochs = 15
+    history = model.fit(X_train, y_train, epochs = epochs)
 
     scores = model.evaluate(X_test, y_test, verbose=0)
     print("mean_squared_logarithmic_error: %.6f" % (scores))
 
-
+    plt.plot()
+    plt.plot(history.history['loss'])
+    plt.xticks(np.arange(0, 15))
+    plt.title('Loss')
+    plt.show()
     # print(model.predict(X_train[10].reshape(1,3)))
     # print(y_train[10])
     return
 
+# 2 mean_squared_logarithmic_error: 0.022235
+# 3 mean_squared_logarithmic_error: 0.015986
+# 4 mean_squared_logarithmic_error: 0.022250
+# 5 mean_squared_logarithmic_error: 0.015907
+# 6 mean_squared_logarithmic_error: 0.015879
+# 7 mean_squared_logarithmic_error: 0.018899
+# 8 mean_squared_logarithmic_error: 0.018779
+# 9 mean_squared_logarithmic_error: 0.018794
+# 10 mean_squared_logarithmic_error: 0.018883
 
 if __name__ == '__main__':
     main()
