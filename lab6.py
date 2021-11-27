bdd6893306de6fe4b3f1b2b8d92c428e5bf027d6
Zhamilya <40009146+zhamilyaa@users.noreply.github.com>
2021-11-28 01:06:52 +0600

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -13,6 +13,8 @@ def main():
     train = data['Angles'].to_numpy()
     labels = data['XY'].to_numpy()
 
+    print(labels)
+
     X = list()
     Y = list()
     for i in range(len(train)):
@@ -26,21 +28,28 @@ def main():
         result = [float(val) for val in labels[i].split(' ')]
         Y.append(result)
 
-    X_train, X_test, y_train, y_test = train_test_split(np.asarray(X), np.asarray(Y), test_size=0.80)
+    X_train, X_test, y_train, y_test = train_test_split(np.asarray(X), np.asarray(Y), train_size=0.80)
 
     print("TRAIN X SHAPE ", np.shape(X_train))
     print("TRAIN Y SHAPE ", np.shape(y_train))
     print("TEST X SHAPE ", np.shape(X_test))
     print("TEST Y SHAPE ", np.shape(y_test))
-    
-    def rmse(y_true, y_pred):
-        return K.sqrt(K.mean(K.square(y_pred-y_true)))
 
+
+    def rmse(y_true, y_pred):
+        return K.sqrt(K.mean(K.square(y_pred - y_true)))
+
+    print(np.shape(X_train))
+    print(np.shape(y_train))
+    print(np.shape(X_test))
+    print(np.shape(y_test))
 
     model = Sequential()
     model.add(Dense(10, input_dim =5, activation = 'tanh'))
     model.add(Dense(16, activation = 'tanh'))
     model.add(Dense(16, activation = 'tanh'))
+
+
     model.add(Dense(3, activation='linear'))
     model.compile(loss='mean_squared_logarithmic_error', optimizer=keras.optimizers.Adam(0.01))
 
@@ -50,7 +59,7 @@ def main():
     print("mean_squared_logarithmic_error: %.6f" % (scores))
 
 
-    print(model.predict(X_train[10].reshape(1,3)))
+    print(model.predict(X_train[10].reshape(1,5)))
     print(y_train[10])
     return
 
